b4b8ce77e380cbcb0ca3b704fd2cae4e764516ed
Howard Li <howard.li237@gmail.com>
2025-04-28 22:13:09 -0700

    added close button

--- src/renderer/components/search/searchResult.tsx
+++ src/renderer/components/search/searchResult.tsx
@@ -11,8 +11,19 @@ interface TabInfo {
   hashtags: string[];
 }
 
-export const SearchResults = () => {
+interface SearchResultProps {
+    setSearchresult: (check: boolean) => void;
+  }
 
+export const SearchResults: React.FC<SearchResultProps> = ({ setSearchresult }) => {
+
+const CloseIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <line x1="18" y1="6" x2="6" y2="18" />
+        <line x1="6" y1="6" x2="18" y2="18" />
+    </svg>
+    );
+      
   return (
     <div className="search-container">
         <Resizable
@@ -24,6 +35,11 @@ export const SearchResults = () => {
             maxWidth={400}
             enable={{ right: true }}
             className="bg-gray-100 p-4">
+            <button 
+            onClick={() => setSearchresult(false)}
+            className="close-button">
+                <CloseIcon/>
+            </button>
             <div>Replace with Search results</div>
         </Resizable>
     </div>
