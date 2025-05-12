48a3fcfdc8760668d6c176024112e9a4e788c9e1
Jiancheng Li <jian128934@gmail.com>
2025-05-11 22:29:53 -0700

    refactor database.ts

--- src/main/database.ts
+++ src/main/database.ts
@@ -1,5 +1,5 @@
 const { DefaultEmbeddingFunction } = require("chromadb");
-import { ChromaClient } from "chromadb";
+import { ChromaClient, Collection, GetResponse } from "chromadb";
 import os from "os";
 
 interface Note {
@@ -9,9 +9,8 @@ interface Note {
   hashtags: string[]
 }
 
-const client = new ChromaClient();
-const modelName = "supabase/gte-small";
-const embedFunc = new DefaultEmbeddingFunction({ model: modelName });
+const defaultModelName = "supabase/gte-small";
+const primaryCollection = "notes";
 
 // Get file or collection ID based on file or directory path
 // Replace all forbidden characters for specifying a collection ID
@@ -34,103 +33,100 @@ const getTagElement = (tag: string): string => {
   return "<span tagname=\"" + tag + "\" data-tag=\"" + tag + "\" class=\"tag-node\">#" + tag + "</span>";
 }
 
-// Add/update embeddings for the specified note
-export const upsertNote = async (notesDirectory: string, filePath: string, content: string, updateHashtags: string[]) => {
-  const docId = getId(filePath, "file");
-  // const dirId = getId(notesDirectory, "dir");
-  // console.log("Updated hashtags:", updateHashtags); // Debugging line
-  try {
-    // Get the notes collection
-    const collection = await client.getOrCreateCollection({
-      name: "Notes",
-      embeddingFunction: embedFunc,
-    });
+// An instance that connects to a collection in the vector database
+export class DbClient {
+  private collection: Collection | null;
+  private collectionName: string;
+  private embedFunc: typeof DefaultEmbeddingFunction;
+  private client: ChromaClient;
 
-    // Add/update notes to vector database
-    await collection.upsert({
-      ids: [docId],
-      documents: [content.toLowerCase()],
-      metadatas: [{
-        hashtags: updateHashtags.join(','),
-        directory: notesDirectory,
-        filePath: filePath,
-      }],
-    });
-
-    // Get document from database for debugging
-    // const results = await collection.get({
-    //   ids: [docId],
-    // });
-    // console.log("Document successfully upserted:", filePath); // Output results
-  } catch (error) {
-    console.error("Error during ChromaDB operation:", error);
+  constructor(collection: string = primaryCollection, model: string = defaultModelName) {
+    this.client = new ChromaClient();
+    this.embedFunc = new DefaultEmbeddingFunction({ model: model });
+    this.collection = null;
+    this.collectionName = collection;
   }
-};
 
-// Deletes the document of the specified note
-export const deleteNote = async (notesDirectory: string, filePath?: string) => {
-  const docId = getId(filePath, "file");
-  try {
-    // Get the notes collection
-    const collection = await client.getOrCreateCollection({
-      name: "Notes",
-      embeddingFunction: embedFunc,
-    });
+  // Initializes connection to the collection. Will be done only once
+  private initCollection = async (collection: string): Promise<void> => {
+    if (!this.collection) {
+      try {
+        this.collection = await this.client.getOrCreateCollection({
+          name: collection,
+          embeddingFunction: this.embedFunc,
+        });
+      } catch (error) {
+        console.error("Error during ChromaDB initialization:", error);
+      }
+    }
+  };
 
-    await collection.delete({
-      ids: [docId],
-      where: {"directory": notesDirectory}
-    });
-  } catch (error) {
-    console.error("Error during ChromaDB operation:", error);
+  // Add/update embeddings for the specified note
+  public upsertNote = async (notesDirectory: string, filePath: string, content: string, updateHashtags: string[]) => {
+    const docId = getId(filePath, "file");
+    // console.log("Updated hashtags:", updateHashtags); // Debugging line
+    try {
+      await this.initCollection(this.collectionName);
+      // Add/update notes to vector database
+      await this.collection.upsert({
+        ids: [docId],
+        documents: [content.toLowerCase()],
+        metadatas: [{
+          hashtags: updateHashtags.join(','),
+          directory: notesDirectory,
+          filePath: filePath,
+        }],
+      });
+
+      // Get document from database for debugging
+      // const results = await this.queryNotes(notesDirectory, content);
+      console.log("Document successfully upserted:", filePath); // Output results
+    } catch (error) {
+      console.error("Error during ChromaDB operation:", error);
+    }
+  };
+
+  // Deletes the document of the specified note in the directory.
+  // If no specific document is specified, deletes all documents of each note in the directory
+  public deleteNote = async (notesDirectory: string, filePath?: string) => {
+    try {
+      await this.initCollection(this.collectionName);
+      // Delete notes from vector database
+      await this.collection.delete({
+        ids: (filePath ? [getId(filePath, "file")] : []),
+        where: {"directory": notesDirectory}
+      });
+    } catch (error) {
+      console.error("Error during ChromaDB operation:", error);
+    }
+  };
+
+  // Return a list of notes that matches the search query 
+  public queryNotes = async (notesDirectory: string, searchQuery: string): Promise<Note[]> => {
+    let data: GetResponse;
+    try {
+      await this.initCollection(this.collectionName);
+      // Query notes by search query
+      data = await this.collection.get({ 
+        where: {"directory": notesDirectory},
+        whereDocument: {"$contains": searchQuery.toLowerCase()}
+      });
+    } catch (error) {
+      console.error("Error during ChromaDB operation:", error);
+    }
+
+    const results = data.ids.map((id: string, index: number) => ({
+      id: id,
+      filePath: String(data.metadatas[index].filePath),
+      content: data.documents[index],
+      hashtags: data.metadatas[index].hashtags ? String(data.metadatas[index].hashtags).split(',') : [],
+    }));
+
+    return results;
+  };
+
+  // Return a list of notes that contains the tag specified in the search query
+  public queryNotesByTag = async (notesDirectory: string, searchQuery: string): Promise<Note[]> => {
+    return this.queryNotes(getTagElement(searchQuery), notesDirectory);
   }
-}
-
-// Queries all notes in the directory
-export const queryAllNotes = async (notesDirectory: string): Promise<Note[]> => {
-  // const dirId = getId(notesDirectory, "dir");
-
-  const collection = await client.getOrCreateCollection({
-    name: "Notes",
-    embeddingFunction: embedFunc,
-  });
-
-  const data = await collection.get({ where: {"directory": notesDirectory} });
-
-  // Transform the queried results into a list of notes
-  const results = data.ids.map((id: string, index: number) => ({
-    id: id,
-    filePath: String(data.metadatas[index].filePath),
-    content: data.documents[index],
-    hashtags: data.metadatas[index].hashtags ? String(data.metadatas[index].hashtags).split(',') : [],
-  }));
-
-  return results;
-}
-
-// Return a list of notes that matches the search query
-export const queryNotes = async (searchQuery: string, notesDirectory: string): Promise<Note[]> => {
-  const collection = await client.getOrCreateCollection({
-    name: "Notes",
-    embeddingFunction: embedFunc,
-  });
-
-  const data = await collection.get({ 
-    where: {"directory": notesDirectory},
-    whereDocument: {"$contains": searchQuery.toLowerCase()}
-  });
-
-  const results = data.ids.map((id: string, index: number) => ({
-    id: id,
-    filePath: String(data.metadatas[index].filePath),
-    content: data.documents[index],
-    hashtags: data.metadatas[index].hashtags ? String(data.metadatas[index].hashtags).split(',') : [],
-  }));
-
-  return results;
-}
-
-// Return a list of notes that contains the tag specified in the search query
-export const queryNotesByTag = async (searchQuery: string, notesDirectory: string): Promise<Note[]> => {
-  return queryNotes(getTagElement(searchQuery), notesDirectory);
 }
--- tests/spec/database.spec.ts
+++ tests/spec/database.spec.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as fs from 'fs';
-import { upsertNote, queryNotes, queryNotesByTag, deleteNote } from '../../src/main/database';
+import { DbClient } from '../../src/main/database';
 
 describe('queryNotes()', () => {
   // Get content of test files
@@ -8,29 +8,29 @@ describe('queryNotes()', () => {
   const file2 = fs.readFileSync(path.join(process.cwd(), 'tests/fixtures/circuits.md'), 'utf8');
   const notesDir = '/tests';
 
+  const database = new DbClient();
+
   it('full text search 1', async () => {
     // Insert test notes into database
-    await upsertNote(notesDir, 'computer.md', String(file1), []);
-    await upsertNote(notesDir, 'circuits.md', String(file2), []);
+    await database.upsertNote(notesDir, 'computer.md', String(file1), []);
+    await database.upsertNote(notesDir, 'circuits.md', String(file2), []);
 
     // Query the note by keyword
-    const data = await queryNotes('cpu socket', notesDir);
+    const data = await database.queryNotes(notesDir, 'cpu socket');
     expect(data[0].filePath).toBe('computer.md');
 
-    await deleteNote(notesDir, 'computer.md');
-    await deleteNote(notesDir, 'circuits.md');
+    await database.deleteNote(notesDir);
   });
 
   it('full text search 2', async () => {
     // Insert test notes into database
-    await upsertNote(notesDir, 'computer.md', String(file1), []);
-    await upsertNote(notesDir, 'circuits.md', String(file2), []);
+    await database.upsertNote(notesDir, 'computer.md', String(file1), []);
+    await database.upsertNote(notesDir, 'circuits.md', String(file2), []);
 
     // Query the note by keyword
-    const data = await queryNotes('kirchhoff', notesDir);
+    const data = await database.queryNotes(notesDir, 'kirchhoff');
     expect(data[0].filePath).toBe('circuits.md');
 
-    await deleteNote(notesDir, 'computer.md');
-    await deleteNote(notesDir, 'circuits.md');
+    await database.deleteNote(notesDir);
   });
 });
